9f6ff32cc927bb8c07e5f435672c1f93d6b9dbb6
Tiago Taveira <tt.taveira@gmail.com>
2021-09-05 21:08:50 +0100

    finished first column of info panel

--- src/components/information-panel/information-panel.component.ts
+++ src/components/information-panel/information-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { GameModelService } from 'src/services/game-model.service';
 import { PlayerVariablesService } from 'src/services/player-variables.service';
 
@@ -14,16 +15,46 @@ export class InformationPanelComponent implements OnInit {
   happiness: number = 0;
 
   PlayerVariables: PlayerVariablesService;
+  Model: GameModelService
 
   constructor(private gameModelService: GameModelService, private playerVariables: PlayerVariablesService) { 
     this.PlayerVariables = playerVariables;
+    this.Model = gameModelService;
   }
   
   
   ngOnInit(): void {
     
     this.value = Math.floor((this.PlayerVariables.co2_emissions * 25) / 100);
-    this.happiness = Math.floor((this.PlayerVariables.utility * 25) / 100);
+    this.happiness =Math.floor(this.PlayerVariables.utility);
+    console.log(this.happiness);
+  }
+
+  public getGdpDiff() {
+    var gdpArray = this.Model.pib_do_ano;
+    var diff = 0;
+    if (gdpArray.length > 2) {
+      diff = gdpArray[gdpArray.length-1] - gdpArray[gdpArray.length-2];
+    } 
+    return diff;
+  }
+
+  public getHappinessDiff() {
+    var happinessArray = this.Model.utilidade_do_ano;
+    var diff = 0;
+    if(happinessArray.length > 2) {
+      diff = happinessArray[happinessArray.length-1] - happinessArray[happinessArray.length-2];
+    }
+    return diff;
+  }
+
+  public getExpenditureDiff() {
+    var expendArray = this.Model.consumo_do_ano;
+    var diff = 0;
+    if (expendArray.length > 2) {
+      diff = expendArray[expendArray.length-1] - expendArray[expendArray.length-2];
+    }
+    return diff;
   }
 
 }
