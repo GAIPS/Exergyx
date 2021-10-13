1f05e0c1617843c75343139e1ca82087f49607b7
Tiago Taveira <tt.taveira@gmail.com>
2021-10-13 16:07:33 +0100

    updated tutorial text

--- src/components/tutorial/tutorial.component.ts
+++ src/components/tutorial/tutorial.component.ts
@@ -17,39 +17,38 @@ export class TutorialComponent implements OnInit {
     {
       title:"Congratulations",
       text:"Due to your skils and knowledge, you have been chosen as the next in line for the portuguese government."
-
     },
     {
       title: "Less Co2 Emissions, More Happiness for the Population",
-      text: "Your mission is to assure that Portugal uses its economical and energetic resources to reach the optimal levels of CO2 Emissions (must reach close to Zero) and Population Happiness (the higher the better) until 2050."
+      text: "Your mission is to assure that Portugal uses its economic and energetic resources to reach the optimal levels of CO2 Emissions (must reach close to Zero) and Population Happiness (the higher the better) until 2050."
     },
     {
       title: "Electrify your Economy to reduce CO2 Emissions",
-      text: "To reduce the CO2 Emissions, it is necessary to exist a significative electrification of your Economy Sectors. (More Eletrification Percentage than Exergy’s)."
+      text: "To reduce the CO2 Emissions, it is necessary to electrify to a significant extent the economic sectors. "
     },
     {
-      title: "More eletrification, More economical growth!",
-      text: "The more electrification you add to those sectors the higher will be the aggregated efficiency, which helps promote the economical growth and therefore helps increasing the usage of energy."
+      title: "More eletrification, More economic growth!",
+      text: "The higher the electrification of economic sectors, the higher the aggregated efficiency, which promotes economic growth. Thus, the increase in efficiency might cause an increase in energy use (rebound effect)."
     },
     {
       title: "How do I get extra energy?",
-      text: "To have extra energy in a reweable manner, you need to invest in instalations of Renewable Power. "
+      text: "To have extra renewable energy, you need to install Renewable Power."
     },
     {
-      title: "How do I invest in Renewable Power?",
-      text: "You can make different decisions, including the installation of infrastructures that inscreases the production of Electric Renewable Power, and rise up the electrification of one of the sectors."
+      title: "What can I do?",
+      text: "You can make different decisions, including the installation of infrastructures to produce electricity, or increase the electrification of each economic sector."
     },
     {
-      title: "Invest more in Renewable Power!",
-      text: "However, the more you invest in energy, the less budget you will have to increase the Expendure value."
+      title: "How much should I invest?",
+      text: "Enough to achieve your targets. However, the more you invest, the less budget you will have for population consumption (expenditure)."
     },
     {
       title: "Make your Population Happier!",
-      text: "The higher the value of Expenditure and the lower the value for the CO2 Emissions are, the higher will be the Population Happiness."
+      text: "The higher the value of expenditure and the lower the value for the CO2 emissions, the higher will be the population happiness."
     }, 
     { 
       title: "Good Luck with your mission!",
-      text:"With your influence, you can help the government taking the necessary actions to insure the success of your misison."
+      text:"With your influence, you can help the government take the necessary actions to ensure the success of your misison."
     }
 
   ]
